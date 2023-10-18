945abbf85d7be9ad3506f1fe3c885e3dc3fda305
DiamondCoder <diamond.coder.backup@gmail.com>
2023-10-18 15:19:33 +0200

    a

--- Lesson7/js/script.js
+++ Lesson7/js/script.js
@@ -1,6 +1,8 @@
 console.log('Hello World!');
 var details = document.querySelector(".details");
-var intro = document.querySelector(".datailsIntro");
+var intro = document.querySelector(".detailsIntro");
+console.log(details);
+console.log(intro);
 
 if (details) {
 	details.style.display = "none";
@@ -14,5 +16,35 @@ if (details) {
 	}
 	
 	more.onclick = showDetails;
+}
+
+/* References */
+
+var items = document.querySelectorAll(".gallery a");
+var viewer = document.querySelector(".imageViewer");
+var viewerImage = viewer.querySelector("img");
+
+console.log(items);
+
+function galleryClick(evt) {
+	evt.preventDefault();
+
+	var href = this.getAttribute("href");
+	viewerImage.setAttribute("src", href);
+	// viewer.style.display = "flex";
+
+	view.classList.add("visible");
+}
+
+function closePreview() {
+	// viewer.style.display = "none";
+	view.classList.remove("visible");
+}
+viewer.onclick = closePreview;
+
+if (items.length > 0) {
+	for (var items of items) {
+		items.onclick = galleryClick();
+	}
 }
 
